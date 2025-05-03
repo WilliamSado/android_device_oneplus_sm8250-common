4891d6de38c28fefe3a7f12230f15d0e483d9855
WilliamSado <williamsado1004@gmail.com>
2025-05-03 22:36:32 +0800

    tmp

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_oplus
