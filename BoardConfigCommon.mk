836dedfcde97eb73ab0a064da60000ff4aa121cd
WilliamSado <williamsado1004@gmail.com>
2025-05-03 22:27:52 +0800

    sm8250-common: Use common compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
